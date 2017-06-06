b65d0d9609bc395c3ccc762114c4e4ea92ae6283
si0 <si00gcf@gmail.com>
2017-06-06 21:53:20 +0900

    DBのSQLを編集

--- databases/schema.sql
+++ databases/schema.sql
@@ -1,8 +1,21 @@
-create table moneyfly (
+create table mf_money (
     id serial primary key,
-    title varchar(45) not null,
+    category integer not null,
     money integer not null,
     note text,
     datetime date not null default 'now',
     created timestamp not null default 'now'
 );
+
+create table mf_category (
+  id serial primary key,
+  category varchar(45) not null
+);
+
+insert into mf_category (category) values ('食費');
+insert into mf_category (category) values ('交通費');
+insert into mf_category (category) values ('雑貨費');
+insert into mf_category (category) values ('書籍費');
+insert into mf_category (category) values ('娯楽費');
+insert into mf_category (category) values ('被服費');
+insert into mf_category (category) values ('定期的支払い料金');
